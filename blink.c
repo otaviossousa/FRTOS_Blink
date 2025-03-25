6d0d442ac7dd4e116804e497be9e4b4e3f8014e5
Otavio Sousa <otaviosousamartins14@gmail.com>
2025-03-25 17:44:25 -0300

    Implement multitasking for LED blinking with FreeRTOS support

--- blink.c
+++ blink.c
@@ -1,29 +1,43 @@
+// Branch: multitasking 
+
 #include "pico/stdlib.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdio.h>
 
 
-const uint LED_PIN_RED = 12;
+const uint LED_PIN_RED = 13;
+const uint LED_PIN_BLUE = 12;
 
-void vBlinkTask()
+void setup()
 {
+    gpio_init(LED_PIN_RED);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_init(LED_PIN_BLUE);
+    gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
+}
+
+void vBlinkTask(void *pvParameters)
+{
+    uint *LED = (uint *)pvParameters;
+    
     for (;;)
     {
-        gpio_put(LED_PIN_RED, 1);
-        vTaskDelay(500);
-        gpio_put(LED_PIN_RED, 0);
-        vTaskDelay(500);
-        printf("Blinking\n");
+        gpio_put(*LED, 1);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        gpio_put(*LED, 0);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
     }
-
 }
 
 int main() 
  {
-    stdio_init_all();
-    gpio_init(LED_PIN_RED);
-    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
-    xTaskCreate(vBlinkTask, "Blink", 128, NULL, 1, NULL);
+    setup();
+    
+    xTaskCreate(vBlinkTask, "Blink Task 1", 128, (void *)&LED_PIN_RED, 1, NULL);
+    xTaskCreate(vBlinkTask, "Blink Task 2", 128, (void *)&LED_PIN_BLUE, 1, NULL);
+    
     vTaskStartScheduler();
+    for (;;)
+        ;
 }
